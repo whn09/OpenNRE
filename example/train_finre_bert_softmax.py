202972e6d6f626ab6bf79c00034ef31d2e7cefad
EC2 Default User <ec2-user@ip-172-16-101-243.ec2.internal>
2020-04-16 10:51:27 +0000

    add train_finre_bert_softmax

---
+++ example/train_finre_bert_softmax.py
@@ -0,0 +1,55 @@
+import sys, json
+import torch
+import os
+import numpy as np
+import opennre
+from opennre import encoder, model, framework
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mask_entity', action='store_true', help='Mask entity mentions')
+args = parser.parse_args()
+
+# Some basic settings
+root_path = '.'
+sys.path.append(root_path)
+if not os.path.exists('ckpt'):
+    os.mkdir('ckpt')
+ckpt = 'ckpt/finre_bert_softmax.pth.tar'
+
+# Check data
+rel2id = json.load(open(os.path.join(root_path, 'benchmark/FinRE/finre_rel2id.json')))
+
+# Define the sentence encoder
+sentence_encoder = opennre.encoder.BERTEncoder(
+    max_length=80, 
+    # pretrain_path=os.path.join(root_path, 'pretrain/albert_base_zh'),  # TODO cannot support now
+    pretrain_path=os.path.join(root_path, 'pretrain/baidubaike'),
+    mask_entity=args.mask_entity
+)
+
+# Define the model
+model = opennre.model.SoftmaxNN(sentence_encoder, len(rel2id), rel2id)
+
+# Define the whole training framework
+framework = opennre.framework.SentenceRE(
+    train_path=os.path.join(root_path, 'benchmark/FinRE/finre_train.txt'),
+    val_path=os.path.join(root_path, 'benchmark/FinRE/finre_valid.txt'),
+    test_path=os.path.join(root_path, 'benchmark/FinRE/finre_test.txt'),
+    model=model,
+    ckpt=ckpt,
+    batch_size=64, # Modify the batch size w.r.t. your device
+    max_epoch=10,
+    lr=2e-5,
+    opt='adamw'
+)
+
+# Train the model
+framework.train_model()
+
+# Test the model
+framework.load_state_dict(torch.load(ckpt)['state_dict'])
+result = framework.eval_model(framework.test_loader)
+
+# Print the result
+print('Accuracy on test set: {}'.format(result['acc']))
